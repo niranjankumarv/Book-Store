9927620ff921468df66ad89af4d56c1ba69aaf43
Niranjankumar <145744622+niranjankumarv@users.noreply.github.com>
2024-03-13 15:49:04 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://niranjan:ninja45@books-collections.nqqnpy8.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Books-collections';
